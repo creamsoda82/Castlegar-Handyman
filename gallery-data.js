4362a4ff97f3dbc011a1123ff895d965ad92fa9e
creamsoda82 <castlegarhandyman@gmail.com>
2026-07-10 14:07:15 -0700

    new photos added

--- gallery-data.js
+++ gallery-data.js
@@ -1,26 +1,58 @@
 /*
   GALLERY PHOTO LIST
   ------------------
-  To add a new photo:
-  1. Upload image into the /gallery folder.
-  2. Add/edit lines below.
-  3. Save & commit.
+  To add more photos later: just add new lines following the same format.
 */
 
 const GALLERY_PHOTOS = [
-  { file: "trex deck.png", caption: "Trex Deck Build" },
+  { file: "trex deck.png", caption: "Trex Deck" },
+  { file: "229 deck.png", caption: "229 Deck Build" },
+  { file: "229 front.png", caption: "229 Front Renovation" },
   { file: "hero-kitchen.png", caption: "Kitchen Renovation" },
 
   // Before & After sets
-  { file: "229 front.png", caption: "229 Front Renovation" },
   { file: "26 front before.png", caption: "26 Front - Before" },
   { file: "26 front after.png", caption: "26 Front - After" },
+  { file: "26 kitchen 1.png", caption: "26 Kitchen Refresh" },
+  { file: "26 kitchen 2.png", caption: "26 Kitchen Refresh 2" },
   { file: "car before.png", caption: "Car Project - Before" },
-  { file: "car after 1.png", caption: "Car Project - After 1" },
+  { file: "car after 1.png", caption: "Car Project - After" },
   { file: "car after 2.png", caption: "Car Project - After 2" },
+  { file: "beetle before.png", caption: "Beetle Project - Before" },
+  { file: "beetle after.png", caption: "Beetle Project - After" },
 
-  // Keep your older ones too
-  { file: "trailer.jpg", caption: "Welding & fabrication" },
-  { file: "deck.jpg", caption: "Deck & pergola carpentry" },
-  { file: "kitchen.jpg", caption: "Kitchen tile & renovation" },
+  // More projects
+  { file: "bus before.png", caption: "Bus Project - Before" },
+  { file: "bus after.png", caption: "Bus Project - After" },
+  { file: "shed before.png", caption: "Shed - Before" },
+  { file: "shed after.png", caption: "Shed - After" },
+  { file: "26 stairs.png", caption: "Stair Project" },
+  { file: "pergola.png", caption: "Pergola Build" },
+  { file: "roof.png", caption: "Roof Work" },
+  { file: "trailer.png", caption: "Trailer Fabrication" },
+  { file: "deck.png", caption: "Deck Carpentry" },
+  { file: "kitchen.png", caption: "Kitchen Tile Work" },
+  { file: "trailer 1.png", caption: "Trailer Work 1" },
+  { file: "trailer 2.png", caption: "Trailer Work 2" },
+
+  // Additional ones
+  { file: "side walk before.png", caption: "Sidewalk - Before" },
+  { file: "side walk after.png", caption: "Sidewalk - After" },
+  { file: "trench plumbing.png", caption: "Trench & Plumbing" },
+  { file: "26 bathroom.png", caption: "Bathroom Reno" },
+  { file: "basement before.png", caption: "Basement - Before" },
+  { file: "basement trench.png", caption: "Basement Trench" },
+  { file: "shop 1.png", caption: "Shop Project 1" },
+  { file: "shop 2.png", caption: "Shop Project 2" },
+  { file: "shop 3.png", caption: "Shop Project 3" },
+  { file: "shop 4.png", caption: "Shop Project 4" },
+  { file: "shop 5.png", caption: "Shop Project 5" },
+  { file: "tile bryan.png", caption: "Tile Work" },
+  { file: "brick.png", caption: "Brick Work" },
+  { file: "counter.png", caption: "Counter Installation" },
+  { file: "floor tile.png", caption: "Floor Tile" },
+  { file: "gate 1.png", caption: "Gate Project" },
+  { file: "gate 2.png", caption: "Gate Project 2" },
+  { file: "tiny home.png", caption: "Tiny Home Build" },
+  { file: "wood siding.png", caption: "Wood Siding" },
 ];
